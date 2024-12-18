49661c083c39c3aac0e97370f1f801e078c06caa
chikaskas <rivaandesai3219@gmail.com>
2024-12-18 14:21:08 +0700

    Fixed good_bot_creator.py

--- good_bot_creator.py
+++ good_bot_creator.py
@@ -5,9 +5,18 @@ def prompt(table: str):
     print(table[0:3])
     print(table[3:6])
     print(table[6:9])
-    return int(input())-1
+    valid = False
+    while not valid:
+        str_input = input()
+        valid = str_input in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+        if valid:
+            if table[int(str_input)] == "-":
+                return int(str_input)
+            else:
+                valid = False
+        print("Invalid input, try again.")
 
-def squares_left(player_char: str, table: str):
+def get_squares_left(player_char: str, table: str):
     squares_left = [
     [0, 1, 2], #row 1
     [3, 4, 5], #row 2
@@ -28,16 +37,20 @@ def squares_left(player_char: str, table: str):
 def choose(table: str):
 
     #picks a square if it will make bot win instantly
-    squares_left = squares_left("X", table)
+    squares_left = get_squares_left("X", table)
     for condition in squares_left:
         if len(condition) == 1:
             return condition[0]
     
     #pick a square if it will stop human win instantly
-    squares_left = squares_left("O", table)
+    squares_left = get_squares_left("O", table)
     for condition in squares_left:
         if len(condition) == 1:
             return condition[0]
+        
+    #pick the first availabe square if less than 3 left
+    if table.count("-") < 3:
+        return table.index("-")
     
     #manually choose a square
     return prompt(table)
@@ -46,23 +59,23 @@ def add_data(data: str):
     bot_file.write(data)
     bot_data += data
 
-def next_scenario(current_scenario: list):
-    if len(current_scenario) == 0: #special case: first scenario
+def next_human_scenario(current_human_scenario: list):
+    if len(current_human_scenario) == 0: #special case: first scenario
         return [0]
     #increase the scenario
-    current_scenario[0] += 1
+    current_human_scenario[0] += 1
     #validate scenario
-    lenght = len(current_scenario)
+    lenght = len(current_human_scenario)
     for index in range(lenght):
         max = 10 +2*index -2*lenght
-        if current_scenario[index] == max:
-            current_scenario[index] = 0
+        if current_human_scenario[index] == max:
+            current_human_scenario[index] = 0
             if index == lenght-1: #special case: enough senarios in class
-                current_scenario.append(0)
-            current_scenario[index+1] += 1
-    if len(current_scenario) == 5: #special case: enough senarios
+                return [0]*(lenght+1)
+            current_human_scenario[index+1] += 1
+    if len(current_human_scenario) == 5: #special case: enough senarios
         return False
-    return current_scenario
+    return current_human_scenario
 
 
 bot_file.close()
