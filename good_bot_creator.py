999b5558278a5b4706192988e1fa02588d7e05c9
chikaskas <rivaandesai3219@gmail.com>
2024-12-20 14:41:42 +0700

    Finished good_bot_creator.py

--- good_bot_creator.py
+++ good_bot_creator.py
@@ -8,8 +8,11 @@ def prompt(table: str):
     valid = False
     while not valid:
         str_input = input()
-        valid = str_input in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+        valid = str_input in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "exit"]
         if valid:
+            if str_input == "exit":
+                bot_file.close()
+                exit()
             if table[int(str_input)] == "-":
                 return int(str_input)
             else:
@@ -35,17 +38,22 @@ def get_squares_left(player_char: str, table: str):
     return squares_left
 
 def choose(table: str):
+    print("Choosing a square...")
 
     #picks a square if it will make bot win instantly
     squares_left = get_squares_left("X", table)
     for condition in squares_left:
-        if len(condition) == 1:
+        if len(condition) == 0:
+            return 0
+        if (len(condition) == 1) and (table[condition[0]] == "-"):
             return condition[0]
     
     #pick a square if it will stop human win instantly
     squares_left = get_squares_left("O", table)
     for condition in squares_left:
-        if len(condition) == 1:
+        if len(condition) == 0:
+            return 0
+        if (len(condition) == 1) and (table[condition[0]] == "-"):
             return condition[0]
         
     #pick the first availabe square if less than 3 left
@@ -56,6 +64,9 @@ def choose(table: str):
     return prompt(table)
 
 def add_data(data: str):
+    global bot_data
+    if not (data in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]):
+        raise ValueError("Invalid input")
     bot_file.write(data)
     bot_data += data
 
@@ -77,5 +88,63 @@ def next_human_scenario(current_human_scenario: list):
         return False
     return current_human_scenario
 
+def get_move(human_scenario: list):
+    min = [1, 9, 57, 249][len(human_scenario)-1]
+    lenght = len(human_scenario)
+    if lenght == 0:
+        index = 0
+    elif lenght == 1:
+        index = 1 + human_scenario[0]
+    elif lenght == 2:
+        index = 9 + human_scenario[1]*6 + human_scenario[0]
+    elif lenght == 3:
+        index = 57 + human_scenario[2]*6*4 + human_scenario[1]*4 + human_scenario[0]
+    elif lenght == 4:
+        index = 249 + human_scenario[3]*6*4*2 + human_scenario[2]*4*2 + human_scenario[1]*2 + human_scenario[0]
+    return int(bot_data[index])
+
+def get_table(human_scenario: list):
+    print(f"Human scenario: {human_scenario}")
+
+    moves_left = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    moves_done = []
+    table = "-"*9
+
+    for index in range(len(human_scenario)-1, -1, -1):
+        moves_done.append(moves_left[get_move(human_scenario[index+1:])]) #bot move
+        moves_left.pop(get_move(human_scenario[index+1:]))
+        moves_done.append(moves_left[human_scenario[index]]) #human move
+        moves_left.pop(human_scenario[index])
+    
+    print(moves_done)
+    print(moves_left)
+
+    bot_to_move = True
+    for move in moves_done:
+        table = table[:move] + ["O", "X"][int(bot_to_move)] + table[move+1:]
+        bot_to_move = not bot_to_move
+    return table
+
+length = len(bot_data)
+leftover = length
+human_scenario = []
+while True:
+    if leftover > 0: #skip already trained senario
+        leftover -= 1
+        human_scenario = next_human_scenario(human_scenario)
+        continue
+
+    table = get_table(human_scenario)
+    choice = choose(table)
+    print(f"Chose {choice}")
+    dashes = 0
+    for index in range(choice):
+        if table[index] == "-":
+            dashes += 1
+    add_data(str(dashes))
+
+    human_scenario = next_human_scenario(human_scenario)
+    if len(human_scenario) == 5:
+        break
 
 bot_file.close()
